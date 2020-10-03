cbbbb6c50873f613f6cf396ed2b89d9e78744d7f
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-03 23:22:31 +0530

    [tejas] class Book name

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Class
+{
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("J.K Rowling", "Harry Potter", 365, 2008);
+            Book book2 = new Book("Dan Brown", "DaVinci Code", 423, 2003);
+
+            Console.WriteLine(book1.author);
+        }
+    }
+}
