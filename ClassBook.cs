cd24749fe7e69312ff6cc3788383e097a73f1a9f
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-03 23:24:27 +0530

    [tejas] added class

---
+++ ClassBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class
+{
+    public class Book
+    {
+        string author = "";
+        string bookName = "";
+        int numberOfPages = 0;
+        int yearOfPublish = 0;
+
+        public Book(string author, string bookName, int numberOfPages, int yearOfPublish)
+        {
+            this.author = author;
+            this.bookName = bookName;
+            this.numberOfPages = numberOfPages;
+            this.yearOfPublish = yearOfPublish;
+        }
+    }
+}
+
